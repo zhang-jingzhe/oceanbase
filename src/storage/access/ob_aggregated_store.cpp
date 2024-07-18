d176c420ed1e09b55bb2b4b326dcd551172e0aad
zjzics <2316228208@qq.com>
2024-07-18 09:38:20 +0800

    initial

--- src/storage/access/ob_aggregated_store.cpp
+++ src/storage/access/ob_aggregated_store.cpp
@@ -452,7 +452,13 @@ int ObAggregatedStore::fill_count(const int64_t row_count)
     ret = OB_INVALID_ARGUMENT;
     STORAGE_LOG(WARN, "Invalid argument to fill count", K(ret), K(row_count));
   } else {
-    LOG_DEBUG("debug to fill row count", K(ret), K(row_count));
+    // LOG_DEBUG("debug to fill row count", K(ret), K(row_count));
+    for (int64_t i = 0; OB_SUCC(ret) && i < agg_row_.get_agg_count(); ++i) {
+      ObAggCell *cell = agg_row_.at(i);
+      if (OB_FAIL(cell->eval(row_buf_.storage_datums_[cell->get_col_offset()]))) {
+        LOG_WARN("Failed to eval agg cell", K(ret), K(i), K(row_buf_), K(*cell));
+      }
+    }
   }
   return ret;
 }
